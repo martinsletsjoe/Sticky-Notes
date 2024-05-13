380f03e4a4513370e5d221e0d8cca8afe06ef41a
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2024-05-13 12:52:42 +0200

    connected to mongodb

--- stickyNotes/src/App.tsx
+++ stickyNotes/src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import StickyNote from "./components/StickyNote";
 
 function App() {
-  // Initialize with an array of unique note indices to render multiple StickyNote components
-  const [stickyNotes, setStickyNotes] = useState([0, 1, 2]);
-
   return (
     <div>
       <StickyNote />
