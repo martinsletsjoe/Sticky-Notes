1e44d08a39873b3d2d603df36c26c59d05e9c4d2
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2024-05-26 12:07:57 +0200

    css and some old code cleanup

--- stickyNotes/src/components/Note.tsx
+++ stickyNotes/src/components/Note.tsx
@@ -20,12 +20,6 @@ const Note = ({ note, onUpdate }) => {
     }
   };
 
-  //   const handleDeleteClick = (event) => {
-  //     event.stopPropagation(); // Stop the event from propagating to other handlers
-  //     event.preventDefault();
-  //     onDelete(note._id);
-  //   };
-
   const handleKeyDown = (event) => {
     if (event.key === "Enter" && !event.shiftKey) {
       event.preventDefault();
@@ -34,33 +28,20 @@ const Note = ({ note, onUpdate }) => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        {/* <div className="stickyNoteContainer"> */}
-        {/* <div className="noteNavBar">
-            <button
-              style={{ background: "none", border: "none", color: "black" }}
-            >
-              +
-            </button>
-            <button type="button" onClick={handleDeleteClick}>
-              x
-            </button>
-          </div> */}
+    <div style={{ display: "flex", flex: 1 }}>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          flex: 1,
+          padding: "8px",
+        }}
+      >
         <textarea
           onKeyDown={handleKeyDown}
           className="noteArea"
           value={text}
           onChange={(e) => setText(e.target.value)}
         ></textarea>
-        {/* <small>
-          {new Date(note.createdAt).toLocaleDateString("no-NO", {
-            month: "long",
-            day: "numeric",
-            year: "numeric",
-          })}
-        </small> */}
-        {/* </div> */}
       </form>
     </div>
   );
